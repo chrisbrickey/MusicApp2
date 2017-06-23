a7a695cd6834d5b04d5fe93c6b12556d798bde07
chrisbrickey <cbrickey1@gmail.com>
2017-06-22 20:20:05 -0700

    complete Users & Sessions controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,26 @@
 class SessionsController < ApplicationController
   def new
+    render :new
   end
 
   def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    redirect_to new_session_url
   end
 
   def create
+    user = User.find_by_credentials( params[:user][:email], params[:user][:password])
+
+    if user.nil?
+      flash[:errors] = ["invalid credentials"]
+      render :new
+    elsif !user.activated?
+      redirect_to root_url, alert: "please activate your account first"
+    else
+      login_user!(user)
+      redirect_to root_url
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,34 @@
 class UsersController < ApplicationController
   def activate
+    @user = User.find_by_activation_token(params[:activation_token])
+    @user.activate!
+    login_user!(@user)
+    flash[:notice] = "account activated!"
+    redirect to root_url
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_session_url
+    else 
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def new
+    @user = User.new
+    render :new
   end
 
   def show
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
